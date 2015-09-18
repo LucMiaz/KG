fe2d80b9756623e309160a6a8d35c4465adcd116
LucMiaz <luc.miaz@gmail.com>
2015-09-18 14:43:23 +0200

    Class GraphicalIntervals
    GraphicalIntervals is a SetOfIntervals with a graphical support. (TODO:
    correct some errors. e.g. adding removing functions; updating of
    intervals (they are not unioning (not very important) but could be the
    sign of other errors.
    
    Corrected some errors in Intervals due to wrong merging.

--- kg/intervals.py
+++ kg/intervals.py
@@ -1,60 +1,54 @@
-<<<<<<< HEAD
-import numpy as np
+import matplotlib
+from matplotlib.widgets import *  
+from pylab import *
 class SetOfIntervals(object):
-=======
-class RangeOfIntervals(object):
->>>>>>> master
-
+    """Define a class of set of intervals, i.e. a closed of R"""
     def __init__(self):
         self.RangeInter=[]
         self.length=0
         self.sorted=False
-    
+        self.lastinterval=None
+        
     def append(self, interv):
         """add a range to the list of intervals.
         need an Interval object"""
         self.sort()
-<<<<<<< HEAD
-        try:
-            interv.get_x()
-            test=True
-        except AttributeError:
-            test= False
-        if test:
-            for i in self.RangeInter:
-                if interv.intersect(i):
-                    a=self.RangeInter.index(i)
-                    interv.union(i)
-                    self.RangeInter.pop(a)
-                    self.length-=1
+        if interv: #check if not None
+            self.remove(interv)
             self.RangeInter.append(interv)
-            self.length+=1
+            self.length=len(self.RangeInter)
             self.sort()
+            self.lastinterval=interv
         return test
-=======
-        for i in self.RangeInter:
-            if interv.intersect(i):
-                a=self.RangeInter.index(i)
-                interv.union(i)
-                self.RangeInter.pop(a)
-                self.length-=1
-        self.RangeInter.append(interv)
-        self.length+=1
-        self.sort()
->>>>>>> master
     
     def sort(self):
+        """Sort the set of Intervals and union adjacent intervals"""
         self.RangeInter.sort()
-        self.sorted=True
+        self.unionize()
+        self.unionize()
     
     def remove(self, bounds):
         """remove the given interval from the list"""
-<<<<<<< HEAD
         if self.haselement(bounds):
             self.RangeInter.remove(bounds)
         else:
             self.removeIntersection(bounds)
-        
+    
+    def unionize(self):
+        """Union intervals if adjacents"""
+        if not self.sorted:
+            self.RangeInter.sort()
+        if self.length>1:
+            for inter in self.RangeInter:
+                k = self.RangeInter.index(inter)
+                if k < self.length-1:
+                    if inter.touch(self.RangeInter[k+1]):
+                        new=inter.union(self.RangeInter[k+1])
+                        self.RangeInter.remove(self.RangeInter[k+1])
+                        self.RangeInter.remove(inter)
+                        self.RangeInter.append(new)
+                        self.length=len(self.RangeInter)
+    
     def removeIntersection(self, bounds):
         """remove the intersection between elements of RangeInter and the Interval bounds)"""
         a=0
@@ -93,24 +87,72 @@ class RangeOfIntervals(object):
             ret.append(self.contains(k))
             k += deltatime
         return ret
-=======
-        self.RangeInter.remove(bounds)
->>>>>>> master
     
     def __repr__(self):
         a=""
         for i in self.RangeInter:
             a=a+" ["+str(i)+"] "
         return a
-<<<<<<< HEAD
-        
-=======
->>>>>>> master
+
     def __str__(self):
         return "Range of intervals. Number of intervals : "+str(self.length)+"\n"+ self.__repr__()
 
+class GraphicalIntervals(SetOfIntervals, AxesWidget):
+    """Set of interval with graphical support"""
+    def __init__(self, ax, useblit = True, **lineprops):
+        """
+        Add intervals to *ax*.  If ``useblit=True``, use the backend-
+        dependent blitting features for faster updates (GTKAgg
+        only for now).  *lineprops* is a dictionary of line properties.
+
+        .. plot :: mpl_examples/widgets/cursor.py
+        """
+        AxesWidget.__init__(self, ax)
+        SetOfIntervals.__init__(self)
+        self.Rectangles=[]
+        self.addremove=True
+        toggle_selector.RS = RectangleSelector(ax, self.onselect, drawtype='line')
+        connect('key_press_event', toggle_selector)
+    
+    def _update(self):
+        """update Rectangles and plot it"""
+        self.Rectangles=[]
+        for rect in self.Rectangles:
+            self.ax.remove(rect)
+        for inter in self.RangeInter:
+            coord=inter.get_x()
+            rect=ax.add_patch(patches.Rectangle((coord[0],0.0), coord[1]-coord[0], 1, alpha=0.4, facecolor="#c7eae5", edgecolor="none"))
+            self.Rectangles.append(rect)
+        self.ax.figure.canvas.draw()
+    
+    def onselect(self, eclick, erelease):
+        """eclick and erelease are matplotlib events at press and release"""
+        if self.addremove:
+            self.LastInterval = self.RangeInter.append(Interval(eclick.xdata,erelease.xdata))
+        else:
+            self.LastInterval=None
+            self.RangeInter.remove(Interval(eclick.xdata,erelease.xdata))
+        self._update()  
+        
+
+    def toggle_selector(self, event):
+        if event.key in ['Q', 'q'] and toggle_selector.RS.active:
+            toggle_selector.RS.set_active(False)
+        if event.key in ['A', 'a'] and not toggle_selector.RS.active:
+            toggle_selector.RS.set_active(True)
+        if event.key in ['\b'] and self.lastinterval:
+            self.remove(self.lastinterval)
+        if event.key in ['r','R']:
+            self.addremove= not self.addremove
+            if self.addremove:
+                print("Adding intervals")
+            else:
+                print("Removing intervals")
+    def __str__(self):
+        return "Graphical representation of : " + SetOfIntervals.__str__(self)
+
 class Interval(object):
-    """create an object of length 2, sorted"""
+    """Create an closed interval. It is an object of length 2, sorted"""
     def __init__(self, xmin,xmax):
         try: 
             xmin==float(xmin)
@@ -122,7 +164,6 @@ class Interval(object):
         self.xmin=min(xmin,xmax)
         self.xmax=max(xmin,xmax)
         
-<<<<<<< HEAD
     def get_x(self):
         return self.xmin, self.xmax
         
@@ -133,29 +174,21 @@ class Interval(object):
     def __str__(self):
         return str(self.xmin) + ', '+ str(self.xmax)
         
-=======
-    def get_min(self):
-        return self.xmin
-    def get_max(self):
-        return self.xmax
-    #representations, string format
-    def __repr__(self):
-        return '{}, {}'.format(self.__class__.__name__, self.xmin, self.xmax)
-    def __str__(self):
-        return str(self.xmin) + ', '+ str(self.xmax)
->>>>>>> master
     #interval operations
     def intersect(self,other):
         """Tells if intervals are intesecting"""
         return not self != other
-<<<<<<< HEAD
+    
+    def touch(self,other):
+        """Tells if intervals touch each other"""
+        return (self.xmin==other.get_x()[1] or self.xmax==other.get_x()[0])
         
     def intersection(self, other):
         """Gives the interval intersection"""
         if self.intersect(other):
             ret=Interval(max(self.xmin,other.xmin),min(self.xmax, other.xmax))
         else:
-            ret=set()
+            ret=None
         return ret
     
     def difference(self, other):
@@ -165,15 +198,15 @@ class Interval(object):
             ret2=Interval(other.get_x()[1],self.xmax)
             if ret1.ispoint():
                 if ret2.ispoint():
-                    return set(), set()
+                    return None, set()
                 else:
-                    return set(), ret2
+                    return None, ret2
             else:
                 return ret1, ret2
         elif self <= other:
-            return Interval(self.xmin, other.get_x()[0]), set()
+            return Interval(self.xmin, other.get_x()[0]), None
         else:
-            return Interval(other.get_x()[1],self.xmax), set()
+            return Interval(other.get_x()[1],self.xmax), None
     
     def ispoint(self):
         """check if interval is only a point"""
@@ -187,24 +220,11 @@ class Interval(object):
         """Return True if self is in other"""
         return ((self <= other) and (self >= other))
         
-=======
-    def intersection(self, other):
-        """Gives the interval intersection"""
-        if self.intersect(other):
-            self.xmax=min(self.xmax, other.xmax)
-            self.xmin=max(self.xmin,other.xmin)
-            return self
-        else:
-            self.xmax=0
-            self.xmin=0
-            return self
->>>>>>> master
     def union(self, other):
         """merge self and other together"""
         self.xmax=max(self.xmax, other.xmax)
         self.xmin=min(self.xmin, other.xmin)
         return self
-<<<<<<< HEAD
         
     #sorting definitions
     def __lt__(self, other):
@@ -226,28 +246,11 @@ class Interval(object):
     def __ge__(self,other):
         return self.xmin >= other.get_x()[0]
     
-R=SetOfIntervals()
-a=Interval(0,1)
-b=Interval(3,4)
-c=Interval(2,2.5)
-d=Interval(2.5,3.5)
-e=Interval(0,1)
-f=Interval(3.6,3.7)
-R.append(a)
-R.append(b)
-=======
-    #sorting definitions
-    def __lt__(self, other):
-        return self.xmax < other.xmin:
-    def __gt__(self,other):
-        return self.xmin>other.xmax:
-    def __eq__(self,other):
-        return 
-    def __ne__(self, other):
-        """Intervals are not intersecting"""
-        return self < other or self > other
-    def __le__(self, other):
-        return self.xmax <= other.xmax:
-    def __ge__(self,other):
-        return self.xmin >= other.xmin:
->>>>>>> master
+x = arange(100)/(99.0)
+y = sin(x)
+fig = figure
+ax = subplot(111)
+ax.plot(x,y)
+
+Hello=GraphicalIntervals(ax)
+show()
