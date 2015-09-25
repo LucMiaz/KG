03f22bbf87a2cd841355d9259f23802ed338bbf8
Enzo SR <enzo.scossa.romano@gmail.com>
2015-09-25 15:59:41 +0200

    fromJson is now working, TODO : adding a second SOI for KG

--- kg/case.py
+++ kg/case.py
@@ -1,16 +1,17 @@
-import sys,os
+import sys,os,pathlib
 import inspect
 from kg.intervals import *
 from kg.measurement_values import measuredValues
 import json
 import time
+import numpy as np
 
 ##Class Case
 class Case(object):
     """
     Defines a case of study
     """
-    def __init__(self, ax, location ,measurement, mID, mic, Tb, Te, author, noiseType):
+    def __init__(self, location, measurement, mID, mic, Tb, Te, author, **kwargs):
         """
         Takes a set of data as dict and an axis to display the data on.
         """
@@ -21,21 +22,20 @@ class Case(object):
                 "mic":  mic,
                 "author": author,
                 "date":time.strftime('%d.%m.%Y'),
-                "tb": Tb,
-                "te": Te,
-                "noiseType":noiseType,
-                "Set": SetOfIntervals()
+                "Tb": Tb,
+                "Te": Te,
+                #"KG": SetOfIntervals(), #Kreischen
+                "Z": SetOfIntervals() #Zischen
                 }
         self.case['caseID'] = str(self)
     
-    def compare(self, otherdisc, zerotime, endtime, deltatime):
+    def compare(self, otherdisc, tmin, tmax, dt, noiseType = 'Z'):
         """Compares the discretization of this case with the one of an algorithm whose results are given in otherdisc. timeparam variable contains the variables for the discretization. Returns a dictionnary with the number of True positives, True negatives, False positives and False negatives"""
         try:
-            discret = selfcase['Set'].discretize(zerotime, endtime, deltatime)
+            disc = self.case[noiseType].discretize(tmin, tmax, dt)
         except:
-            discret = 0
-        disc = self.discretize(timeparam)
-        assert len(otherdisc) == len(disc)
+            disc = 0
+        assert( len(otherdisc) == len(disc) )
         retTF={'FP':[], 'TP':[], 'FN':[],'TN':[]}
         retTF['TP'] = np.logical_and(otherdisc,disc)
         retTF['TN'] = np.logical_and(np.logical_not(otherdisc), np.logical_not(disc))
@@ -45,25 +45,29 @@ class Case(object):
         
     def save(self, mesPath):
         '''
-        save Case to file
+        save Case to file in mespath\test_cases\author\case_mID_mic_author.json
         '''
         mesPath = pathlib.Path(mesPath)
         casePath = mesPath.joinpath('test_cases').joinpath(self.case['author'])
         os.makedirs(casePath.as_posix(), exist_ok = True)
         name = str(self) + '.json'
         casePath = casePath.joinpath(name)
-        self.toJSON(casepath)
+        self.toJSON(casePath)
+        return(casePath.as_posix())
+        
+    def get_SOI(self, noiseType='Z'):
+        return(self.case[noiseType])
     
     def __str__(self):
         """prints the name of the case"""
-        return( "case_"+str(self.case['mID'])+"_"+str(self.case['mic'])+\
+        return( "case_"+str(self.case['mID'])+"_"+str(self.case['mic'])+'_'+\
         str(self.case['author']))
     
     def __repr__(self):
         """gives a representation of the case"""
         return self.toJSON()
 
-    def toJSON(self,filename=None):
+    def toJSON(self,filename = None):
         """returns the essential informations of self, if Pathname or Path is given, save in file."""
         if filename:
             if not isinstance(filename, pathlib.Path):
@@ -80,9 +84,15 @@ class Case(object):
         """@classmethod is used to pass the class to the method as implicit argument. Then we open a file in JSON located at casePath and give it to the class with **kwargs (meaning that we pass an arbitrary number of arguments to the class)
         """
         try:
-            return cls(**json.load(open(casePath, 'r')))
+            dict = json.load(open(casePath, 'r'))
         except FileNotFoundError:
             raise Error("The file in path" + casePath + " has not be found.")
+        cl = cls(**dict)
+        for nT in ['Z']:# todo :'KG' 
+            dNT = dict[nT]["SetOfIntervals"]
+            set = [[int['xmin'] , int['xmax']] for int in dNT] 
+            cl.get_SOI(nT).appendlistofduples(set)
+        return(cl)
 
 ## Test
 if __name__ == "__main__":
@@ -92,6 +102,12 @@ if __name__ == "__main__":
     y = np.sin(x)
     f, ax = plt.subplots(1)
     ax.plot(x,y)
+    plt.show()
     #new = GraphicalIntervals(ax)
-    Newcase=Case(ax,'Zug','Vormessung','m_0100','1',0,10,'luc','Z')
-    GraphicalIntervalsHandle(ax,Newcase.case['Set'])
+    Newcase = Case('Zug','Vormessung','m_0100','1',0,10,'esr')
+    GraphicalIntervalsHandle(ax, Newcase.get_SOI())
+    ##save
+    mesPath = 'Measurements_example\MBBMZugExample'
+    casePath = Newcase.save(mesPath)
+    Newcase2 = Case.from_JSON(casePath)
+    
